821613c3ae046cbbf55862574e1e8df1f4a2e2d5
Sándor Dér <der.sandor72@gmail.com>
2025-02-14 12:43:08 +0100

    Új funkció vagy javítás

--- connectmate/urls.py
+++ connectmate/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView  # 🔥 JWT token nézetek importálása
+from django.http import JsonResponse  # 🔥 Hozzáadjuk a JsonResponse importot
+
+# 🔥 Alapértelmezett gyökér URL nézet (ha valaki a http://127.0.0.1:8000/ oldalt nyitja meg)
+def home_view(request):
+    return JsonResponse({"message": "Üdvözöllek a ConnectMate API-nál! Használd az /api/ végpontokat."})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +16,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 🔥 Token generálás útvonal
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 🔥 Token frissítés útvonal
     path('api/messages/', include('messaging.urls')),  # Üzenetküldési API hozzáadása
+    path('', home_view),  # 🔥 Most már definiált home_view nézetet használunk!
+    
 ]
 
 from django.conf import settings
